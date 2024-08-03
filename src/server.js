3104c472eeff8f008a22ce7a0d75a71e3798f04e
ShivamIT23 <138220952+ShivamIT23@users.noreply.github.com>
2024-08-03 17:54:45 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const indexRouter = require("../Routes/index")
 const cors = require("cors");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(cors());
